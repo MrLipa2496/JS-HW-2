59dd7324d8dafd268c34d7adae98f9e5ef42ff95
MrLipa2496 <sasalipa216@gmail.com>
2024-05-13 10:42:13 +0300

    Finish Ex-1-if

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// Завдання на if
+
+// 1
+
+const isDay = false;
+
+if (isDay) {
+  document.write("<p class = 'light-theme'>Світла тема</p>");
+} else {
+  document.write("<p class = 'dark-theme'>Темна тема</p>");
+}
